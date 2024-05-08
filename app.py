1a5a3732bc63e44d123cd324ff4f409ad1cac36d
mitali-7 <mitalig2107@gmail.com>
2024-05-08 20:42:16 +0530

    Created Weather API

---
+++ app.py
@@ -0,0 +1,17 @@
+import requests
+api_key = '21f264c87538384d01870f19ec2e087b'
+
+city = input('Enter city name: ')
+
+url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    temp = data['main']['temp']
+    desc = data['weather'][0]['description']
+    print(f'Temperature: {temp} K')
+    print(f'Description: {desc}')
+else:
+    print('Error fetching weather data')
